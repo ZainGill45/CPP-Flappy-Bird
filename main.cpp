b94da3262367659f7642f92c8253d63845bb30e4
Zain Gill <zaingill45@gmail.com>
2025-04-22 13:33:01 -0600

    Started drawing debug text on screen and got player to move around with keyboard WASD input.

--- main.cpp
+++ main.cpp
@@ -3,17 +3,32 @@
 
 int main(int, char**) {
     InitWindow(800, 600, "My Raylib Window");
-    SetTargetFPS(60);
+    SetTargetFPS(300);
+
+    Vector2 playerPosition = { 400, 300 };
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText("Hello, from learning C++", 10, 10, 20, BLACK);
+        Vector2 inputVector = { 0, 0 };
 
-        DrawFPS(10, 40);
+        if (IsKeyDown(KEY_W)) inputVector.y = -1;
+        if (IsKeyDown(KEY_S)) inputVector.y = 1;
+        if (IsKeyDown(KEY_D)) inputVector.x = 1;
+        if (IsKeyDown(KEY_A)) inputVector.x = -1;
 
+        playerPosition.x += inputVector.x;
+        playerPosition.y += inputVector.y;
+
+        DrawCircle(playerPosition.x, playerPosition.y, 10, RED);
+
+        char debugText[32];
+        snprintf(debugText, sizeof(debugText), "(%.0f, %.0f)", inputVector.x, inputVector.y);
+        DrawText(debugText, 10, 35, 24, BLACK);
+
+        DrawFPS(10, 5);
         EndDrawing();
     }
 
