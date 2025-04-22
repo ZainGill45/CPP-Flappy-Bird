ed0007682c8c03469607a604198d628f1fcf6a95
Zain Gill <zaingill45@gmail.com>
2025-04-22 17:26:45 -0600

    Implemented game over texture when the player goes out of bounds

--- main.cpp
+++ main.cpp
@@ -8,58 +8,57 @@ int main(int, char**) {
     SetTargetFPS(300);
 
     // Load spries for the game
-    Texture2D background = LoadTexture("./assets/sprites/background-day.png");
-    if (background.id == 0) TraceLog(LOG_ERROR, "Failed to load background texture");
-    Texture2D player = LoadTexture("./assets/sprites/yellowbird-upflap.png");
-    if (player.id == 0) TraceLog(LOG_ERROR, "Failed to load player texture");
+    const Texture2D BACKGROUND_TEXTURE = LoadTexture("./assets/sprites/background-day.png");
+    if (BACKGROUND_TEXTURE.id == 0) TraceLog(LOG_ERROR, "Failed to load background texture");
+    const Texture2D GAMEOVER_TEXTURE = LoadTexture("./assets/sprites/gameover.png");
+    if (GAMEOVER_TEXTURE.id == 0) TraceLog(LOG_ERROR, "Failed to load gameOver texture");
+    const Texture2D PLAYER_TEXTURE = LoadTexture("./assets/sprites/yellowbird-upflap.png");
+    if (PLAYER_TEXTURE.id == 0) TraceLog(LOG_ERROR, "Failed to load player texture");
 
-    SetTextureWrap(background, TEXTURE_WRAP_REPEAT);
+    SetTextureWrap(BACKGROUND_TEXTURE, TEXTURE_WRAP_REPEAT);
 
-    // Retrieve screen size
-    const int screenWidth = GetScreenWidth();
-    const int screenHeight = GetScreenHeight();
+    const int SCREEN_WIDTH = GetScreenWidth();
+    const int SCREEN_HEIGHT = GetScreenHeight();
+    int CENTERED_GAMEOVER_TEXTURE_X_POSITION = (SCREEN_WIDTH * 0.5f) - (GAMEOVER_TEXTURE.width * 0.5f);
+    int CENTERED_GAMEOVER_TEXTURE_Y_POSITION = (SCREEN_HEIGHT * 0.5f) - (GAMEOVER_TEXTURE.height * 0.5f);
 
-    // Player setup
+    const float PLAYER_JUMP_FORCE = -700.0f;
+    const float GRAVITY = 9.84f;
+    const int PLAYER_SIZE = 25;
+
     Vector2 playerPosition = { 128, 256 };
+    float yVelocity = 0;
+    bool gameOver = false;
 
-    float yVelocity;
-
-    const float playerJumpForce = -700.0f;
-    const float gravity = 9.84f;
-    const int playerSize = 25;
-
-    while (WindowShouldClose() == false)
+    while (!WindowShouldClose())
     {
-        // Start frame
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Draw tiled background in one call
-        Rectangle backgroundRectangle = { 0.0f, 0.0f, (float)screenWidth, (float)screenHeight };
-        DrawTextureRec(background, backgroundRectangle, { 0, 0 }, WHITE);
+        Rectangle backgroundRectangle = { 0.0f, 0.0f, (float)SCREEN_WIDTH, (float)SCREEN_HEIGHT };
+        DrawTextureRec(BACKGROUND_TEXTURE, backgroundRectangle, { 0, 0 }, WHITE);
 
-        // Handle input and movement
         if (IsKeyPressed(KEY_SPACE)) {
             yVelocity = 0.0f;
-            yVelocity += playerJumpForce;
+            yVelocity += PLAYER_JUMP_FORCE;
         }
 
-        yVelocity += gravity;
+        yVelocity += GRAVITY;
         
-        // Update player position
         playerPosition.y += yVelocity * GetFrameTime();
 
-        playerPosition.x = Clamp(playerPosition.x, 0, screenWidth  - playerSize);
-        playerPosition.y = Clamp(playerPosition.y, 0, screenHeight - playerSize);
+        if (playerPosition.y <= 0 || playerPosition.y >= SCREEN_HEIGHT - PLAYER_SIZE) {
+            DrawTexture(GAMEOVER_TEXTURE, CENTERED_GAMEOVER_TEXTURE_X_POSITION, CENTERED_GAMEOVER_TEXTURE_Y_POSITION, WHITE);
+            playerPosition.y = Clamp(playerPosition.y, 0, SCREEN_HEIGHT - PLAYER_SIZE);
+            gameOver = true;
+        }
 
-        // Draw player and debug info
-        DrawTexture(player, playerPosition.x, playerPosition.y, WHITE);
+        DrawTexture(PLAYER_TEXTURE, playerPosition.x, playerPosition.y, WHITE);
 
-        // End frame
         EndDrawing();
     }
 
-    UnloadTexture(background);
+    UnloadTexture(BACKGROUND_TEXTURE);
     CloseWindow();
     return 0;
 }
