cb659b616fc9f3a7ecc436a7df10235bc5775b08
Zain Gill <zaingill45@gmail.com>
2025-04-22 14:18:48 -0600

    Made it so that the player can't go out of the bounds of the window

--- main.cpp
+++ main.cpp
@@ -3,35 +3,42 @@
 #include <raymath.h>
 
 int main(int, char**) {
-    InitWindow(800, 600, "Leaning C++");
+    InitWindow(1280, 720, "Leaning C++");
     SetTargetFPS(300);
 
     Vector2 playerPosition = { 400, 300 };
 
     float playerSpeed = 100;
 
+    int playerRadius = 10;
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        int screenWidth = GetScreenWidth();
+        int screenHeight = GetScreenHeight();
+
         Vector2 inputVector = { 0, 0 };
 
-        if (IsKeyDown(KEY_W)) inputVector.y = -1;
-        if (IsKeyDown(KEY_S)) inputVector.y = 1;
-        if (IsKeyDown(KEY_D)) inputVector.x = 1;
-        if (IsKeyDown(KEY_A)) inputVector.x = -1;
+        if (IsKeyDown(KEY_W)) inputVector.y += -1;
+        if (IsKeyDown(KEY_S)) inputVector.y += 1;
+        if (IsKeyDown(KEY_D)) inputVector.x += 1;
+        if (IsKeyDown(KEY_A)) inputVector.x += -1;
 
         inputVector = Vector2Normalize(inputVector);
 
         playerPosition.x += inputVector.x * playerSpeed * GetFrameTime();
         playerPosition.y += inputVector.y * playerSpeed * GetFrameTime();
 
-        DrawCircle(playerPosition.x, playerPosition.y, 10, RED);
+        playerPosition = { Clamp(playerPosition.x, 0 + playerRadius , screenWidth - playerRadius), Clamp(playerPosition.y, 0 + playerRadius, screenHeight - playerRadius) };
+
+        DrawCircle(playerPosition.x, playerPosition.y, playerRadius, RED);
 
         char debugText[32];
         snprintf(debugText, sizeof(debugText), "(%.0f, %.0f)", inputVector.x, inputVector.y);
-        DrawText(debugText, 10, 35, 24, BLACK);
+        DrawText(debugText, 10, 32, 20, BLACK);
 
         DrawFPS(10, 5);
         EndDrawing();
